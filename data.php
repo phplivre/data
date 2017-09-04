08d277a2a3ce81f4afaf4feecf03f7298e614ab9
php livre <sac.phplivre@gmail.com>
2017-09-04 14:31:58 -0300

    Create data.php

---
+++ data.php
@@ -0,0 +1,37 @@
+<?php
+function data() {
+$data = date('D');
+$mes = date('M');
+$dia = date('d');
+$ano = date('Y');
+ 
+$semana = array(
+'Sun' => 'Domingo',
+'Mon' => 'Segunda-Feira',
+'Tue' => 'Terca-Feira',
+'Wed' => 'Quarta-Feira',
+'Thu' => 'Quinta-Feira',
+'Fri' => 'Sexta-Feira',
+'Sat' => 'Sábado'
+);
+ 
+$mes_extenso = array(
+'Jan' => 'Janeiro',
+'Feb' => 'Fevereiro',
+'Mar' => 'Marco',
+'Apr' => 'Abril',
+'May' => 'Maio',
+'Jun' => 'Junho',
+'Jul' => 'Julho',
+'Aug' => 'Agosto',
+'Nov' => 'Novembro',
+'Sep' => 'Setembro',
+'Oct' => 'Outubro',
+'Dec' => 'Dezembro'
+);
+
+return $semana["$data"] . ", {$dia} de " . $mes_extenso["$mes"] . " de {$ano}" . " às " . date('H:i:s');
+}
+
+echo data();
+?>
